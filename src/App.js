ac8ad008fb8efcf62fc0e69d296a0264290f4efd
utkarsh00047 <utkarsh.gupta@bhavnacorp.com>
2023-02-06 19:58:44 +0530

    ini

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {Header} from './Components/Header/Header'
+import { Sidebar } from './Components/Sidebar/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <>
+      <header>
+        <h2>Header</h2>
+      </header>
+  </>
+  )
+}
