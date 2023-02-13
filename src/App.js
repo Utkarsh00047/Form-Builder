1933a3e8270eeb130d49f239d5bbdeac9183584c
Anjali27bhavna <anjali.saini@bhavnacorp.com>
2023-02-13 11:41:10 +0530

    no changes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import {Header} from './Components/Header/Header'
 import { Sidebar } from './Components/Sidebar/Sidebar';
-import { Form } from './Components/Form/Form';
-import { Accordination } from './Components/Accordination/Accordination';
+// import { Form } from './Components/Form/Form';
+// import { Accordination } from './Components/Accordination/Accordination';
+import { DropContainer } from './Components/Drop Container/DropContainer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Sidebar/>
-      <Form/>
-      <Accordination/>
+      <DropContainer />
+      {/* <Form/> */}
+      {/* <Accordination/> */}
     </div>
   );
 }
